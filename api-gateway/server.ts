07e8d35c79bbff8999084b49ffb5b75f7daad53f
Marzooq13579 <marzooqabs@gmail.com>
2024-03-13 17:23:52 +0530

    Initalized api gateway server

---
+++ api-gateway/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Welcome to Api Gateway");
+});
+
+app
+  .listen(PORT, () => {
+    console.log("Server Running at PORT", PORT);
+  })
+  .on("error", (err) => {
+    throw new Error(err.message);
+  });
